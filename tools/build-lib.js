f96a5a89670ea7507655b36888b1c11f0c5f2689
Jonatan Salas <jonatansalas@live.com.ar>
2016-07-22 15:14:19 -0300

    Add folder structure Add babelrc config file Add gitignore Add travis.yml Added more dependencies to package.json Added tools folder

---
+++ tools/build-lib.js
@@ -0,0 +1,22 @@
+import path from 'path';
+import fsep from 'fs-extra-promise';
+import rimraf from 'rimraf-promise';
+import colors from 'colors';
+import { exec } from 'child-process-promise';
+
+const repoRoot = path.resolve(__dirname, '../');
+const lib = path.join(repoRoot, 'lib');
+const lessSrc = path.join(repoRoot, '/src/styles');
+const lessDest = path.join(lib, '/styles');
+
+console.log('building lib'.green);
+
+rimraf(lib)
+    .then(function (error) {
+        let babelCli = '"./node_modules/.bin/babel" src -d lib';
+        return exec(babelCli).fail(function (error) {
+            console.log(colors.red(error))
+        });
+    })
+    .then(() => fsep.copyAsync(lessSrc, lessDest))
+    .then(() => console.log('lib built'.green));

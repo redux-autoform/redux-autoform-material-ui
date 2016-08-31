bc1d9812563e6864c1a93146bb0ff60d132bec6f
Jonatan Salas <jonatansalas@live.com.ar>
2016-08-30 23:03:59 -0300

    #8 Added support for radio buttons Minimal fixes on the demo less file

--- src/components/field/Radio.js
+++ src/components/field/Radio.js
@@ -1,35 +1,47 @@
 import React, { Component, PropTypes } from 'react';
+import { RadioButton, RadioButtonGroup } from 'material-ui';
 
 class Radio extends Component {
-    // handleChange = (event) => {
-    //     let { onChange } = this.props;
-    //     onChange(event.target.value);
-    // };
-    //
-    // getOptions = (value) => {
-    //     let { options, name, fieldLayout } = this.props;
-    //
-    //     // these props don't vary per item
-    //     let invariantRadioProps = { inline: fieldLayout == 'inline', name, onChange: this.handleChange };
-    //
-    //     return options.map((item, index) => (
-    //         <BootstrapRadio key={index} value={item.value}  checked={item.value == value} {...invariantRadioProps}>
-    //             {item.text}
-    //         </BootstrapRadio>
-    //     ))
-    // };
-    //
-    // render() {
-    //     let { error, touched, displayName, name, help, fieldLayout, innerSize, value } = this.props;
-    //     let formGroupProps = { error, touched, displayName, name, help, fieldLayout, innerSize };
-    //     let options = this.getOptions(value);
-    //
-    //     return (
-    //         <FormGroup {...formGroupProps} >
-    //             { options }
-    //         </FormGroup>
-    //     )
-    // }
+    handleChange = (event, value) => {
+        let { onChange } = this.props;
+        onChange(value);
+    };
+
+    getOptions = () => {
+        let { options, name, fieldLayout } = this.props;
+
+        // // these props don't vary per item
+        // let invariantRadioProps = { inline: fieldLayout == 'inline', name, onChange: this.handleChange };
+
+        return options.map(({text, value}, index) => (
+            <RadioButton key={index} label={text} value={value} onChange={this.handleChange}/>
+        ))
+    };
+
+    render() {
+        let { error, touched, displayName, name, help, fieldLayout, innerSize, value } = this.props;
+        // let formGroupProps = { error, touched, displayName, name, help, fieldLayout, innerSize };
+        let helpBlock = null;
+        let nameBlock = null;
+
+        if (help) {
+            helpBlock = <h5 style={{color: "#9e9e9e"}}>{help}</h5>;
+        }
+
+        if (displayName) {
+            nameBlock = <h4 style={{color: "#757575"}}>{displayName}</h4>;
+        }
+
+        return (
+            <div>
+                {nameBlock}
+                <RadioButtonGroup name={name} valueSelected={value}>
+                    {this.getOptions()}
+                </RadioButtonGroup>
+                {helpBlock}
+            </div>
+        )
+    }
 }
 
 export default Radio;

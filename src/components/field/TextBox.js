286d48ad0e09e4a7be5dc0d82a7809aec74195df
jonisaa <jonatansalas@live.com.ar>
2016-09-02 11:22:21 -0300

    #12 Revert with support for Addon Before and After

--- src/components/field/TextBox.js
+++ src/components/field/TextBox.js
@@ -19,26 +19,7 @@ class TextBox extends Component {
     };
 
     render() {
-        let {value, error, displayName, name, onChange, placeholder, addonBefore, addonAfter} = this.props;
-        let before = null, after = null, textField = "col-md-12";
-
-	    if (addonBefore) {
-		    textField = "col-md-8";
-			before = (
-				<div className="col-md-4">
-					<p className="center-block">{addonBefore}</p>
-				</div>
-			)
-	    }
-
-	    if (addonAfter) {
-		    textField = "col-md-8";
-		    after = (
-			    <div className="col-md-4">
-				    <p className="center-block">{addonAfter}</p>
-			    </div>
-		    )
-	    }
+        let { value, error, displayName, name, onChange, placeholder } = this.props;
 
         return (
 			<TextField

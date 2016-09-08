e9c2990af97e0a70fd9afa70c77eb29db11592f2
Jonatan Salas <jonatansalas@live.com.ar>
2016-09-08 19:59:12 -0300

    #7 Fixing Lookup and Refactoring presets

--- src/components/field/Lookup.js
+++ src/components/field/Lookup.js
@@ -9,43 +9,45 @@ class Lookup extends Component {
         source: []
     };
 
-    onChange = (chosenRequest, index) => {
+    onNewRequest = (text, index) => {
         let { onChange, options } = this.props;
         let { valueKey } = options;
         let { source } = this.state;
-        let value;
+        let item = null;
 
-        if (valueKey && index !== -1) {
-            value = source[index][valueKey];
-            onChange(value);
+        if (index !== -1) {
+            item = source[index][valueKey];
         }
-        else if (index !== -1) {
-            value = source[index]['value'];
-            onChange(value);
-        }
+
+        onChange(item);
+    };
+
+    onUpdateInput = (searchText, dataSource) => {
+        let { onChange, options } = this.props;
+        let { labelKey, valueKey } = options;
+
+        let item = dataSource.find(item => {
+            if (item[labelKey] === searchText) {
+                return item[valueKey];
+            }
+
+            return {};
+        });
+
+        onChange(item);
     };
 
     setDataSource = () => {
         let { options } = this.props;
-        let { url, labelKey, valueKey, itemsKey } = options;
+        let { url, labelKey, valueKey } = options;
         let { urlOptions, urlErrors } = this.state;
 
         if (Array.isArray(options)) {
-            let dataSource = options.map(({ value, label }, index) => ({
-                text: label,
-                value: value
-            }));
-
-            this.setState({ source: dataSource });
+            this.fillDataSource(options);
         }
-        else if (url && labelKey && valueKey && itemsKey) {
+        else if (url && labelKey && valueKey) {
             if (Array.isArray(urlOptions)) {
-                let dataSource = urlOptions.map((item, index) => ({
-                    text: item[labelKey],
-                    value: item[valueKey]
-                }));
-
-                this.setState({ source: dataSource });
+                this.fillDataSource(urlOptions, labelKey, valueKey);
             }
             else if (urlErrors) {
                 console.error(
@@ -56,16 +58,30 @@ class Lookup extends Component {
         }
     };
 
+    fillDataSource = (arr, label = 'label', value = 'value') => {
+        let source = arr.map(item => ({
+            text: item[label],
+            value: item[value]
+        }));
+
+        this.setState({source: source});
+    };
+
     componentWillMount() {
         let { options } = this.props;
         let { url } = options;
 
         if (url) {
-            callApi(url).then(response => response.json())
-                .then(response => this.setState({ urlOptions: response }));
+            callApi(url)
+                .then(response => response.json())
+                .then(response => {
+                    this.setState({ urlOptions: response });
+                    this.setDataSource();
+                });
         }
-
-        this.setDataSource();
+        else {
+            this.setDataSource();
+        }
     }
 
     render() {
@@ -82,7 +98,6 @@ class Lookup extends Component {
             )
         }
 
-        console.info("This is the source array => " + JSON.stringify(source, null, 2));
         console.info("AutoComplete - This are the props => " + JSON.stringify(Object.keys(this.props), null, 2));
 
         return (
@@ -93,7 +108,8 @@ class Lookup extends Component {
                     floatingLabelText={displayName}
                     dataSource={source}
                     hintText={placeholder}
-                    onNewRequest={this.onChange}
+                    onNewRequest={this.onNewRequest}
+                    onUpdateInput={this.onUpdateInput}
                     onBlur={onBlur}
                     floatingLabelFixed
                     fullWidth

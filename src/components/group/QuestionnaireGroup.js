cb537551531d336c7782437f2acb67336e8d3301
Jonatan Salas <jonatansalas@live.com.ar>
2016-09-10 19:25:12 -0300

    #35 Support for Questionnaire forms is already done

--- src/components/group/QuestionnaireGroup.js
+++ src/components/group/QuestionnaireGroup.js
@@ -1,18 +1,61 @@
 import React, { Component, PropTypes } from 'react';
 import BaseGroup from './BaseGroup';
 import { LinearProgress } from 'material-ui';
+import Arrays from '../../util/Arrays';
 
 class QuestionnaireGroup extends BaseGroup {
 
     state = {
-        percentage: 10
+        size: this.props.fields.length,
+        percentage: 0
     };
+
+    filterValues = (fields) => {
+        let arr = Arrays.mergeJson(fields.map(field => {
+            let { value, touched, name } = field.reduxFormProps;
+
+            if (touched) {
+                return (value && !(value === ''))? {[name]: value} : null
+            }
+
+            return null;
+        }));
+
+        return Object.keys(arr);
+    };
+
+    calculatePercentage = (fields) => {
+        let { size } = this.state;
+
+        let filteredValues = this.filterValues(fields);
+        let completedFields = filteredValues.length;
+
+        this.setState({
+            percentage: ((completedFields * 100) / size)
+        });
+    };
+
+    componentDidMount() {
+        let { fields } = this.props;
+
+        this.calculatePercentage(fields);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (!Object.is(nextProps, this.props)) {
+            let { fields } = nextProps;
+
+            this.calculatePercentage(fields);
+        }
+    }
 
     render() {
         let { percentage } = this.state;
 
         let header = this.getHeader();
         let content = this.getContent();
+
+        console.info("This is the current percentage => " + percentage);
 
         return (
             <section>

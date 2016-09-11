31ada5416ab4871d3ddb38859928125c586c3d8c
Jonatan Salas <jonatansalas@live.com.ar>
2016-09-11 00:22:16 -0300

    Min diff

--- src/util/wrapUpdate.js
+++ src/util/wrapUpdate.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import shallowCompare from 'react-addons-shallow-compare';
 
 export default function wrapUpdate(WrapperComponent) {
-    return class ShouldUpdateComponent extends React.Component {
+    return class ShouldUpdateComponent extends Component {
         shouldComponentUpdate(nextProps, nextState) {
             return shallowCompare(this, nextProps, nextState);
         }
